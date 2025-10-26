4b4c83f703510e4790b13bdc336f6e4f2e91ea83
Moeenc137 <moeenc137@gmail.com>
2025-10-26 22:54:14 +0500

    arrays

---
+++ script.js
@@ -0,0 +1,36 @@
+//arrays:
+
+let marks = [90, 87, 33, 54, 23, 59];
+
+let students = ["Bilal", "Ahmed", "Moiz", "Saim"];
+
+console.log(marks);
+console.log(marks.length);
+
+//array indices:
+
+console.log(marks[2]); //to acess an array item
+console.log((marks[3] = 78)); // to change an array item
+
+//looping in arrays
+
+//for loop:
+
+for (idx = 0; idx < students.length; idx++) {
+  console.log(students[idx]);
+}
+
+//for-of loop:
+
+for (let stu of students) {
+  console.log(stu);
+}
+
+//practice:
+
+//find average of all the students marks in the class:
+
+for (i = 0; i < marks.length; i++) {
+  hg;
+  console.log(i + marks);
+}
