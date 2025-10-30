8140b9b2482ed6badebac80b4f019515b2aff4f5
Moeenc137 <moeenc137@gmail.com>
2025-10-31 01:45:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ for (idx = 0; idx < students.length; idx++) {
 }
 
 //for-of loop:
-
 for (let stu of students) {
   console.log(stu);
 }
@@ -30,7 +29,24 @@ for (let stu of students) {
 
 //find average of all the students marks in the class:
 
-for (i = 0; i < marks.length; i++) {
-  hg;
-  console.log(i + marks);
+sum = 0;
+
+for (val of marks) {
+  sum += val;
+}
+
+let avg = val / marks.length;
+
+console.log(avg);
+
+//add minus 10% offer in every item:
+
+let items = [250, 645, 300, 900, 50];
+
+let mult = 10;
+
+for (idx0 = 0; idx0 < items.length; idx0++) {
+  let offer = items[idx0] / 10;
+  items[idx0] -= offer;
+  console.log(`price after 10% offer on item: ${items[idx0]}`);
 }
